a84f7319000841808218dea9963af339aa04f706
user <user@es302n34>
2018-10-02 11:03:30 +0800

    initial

--- exam/src/com/fju/Hello.java
+++ exam/src/com/fju/Hello.java
@@ -4,6 +4,11 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		String name = "Joyce";
+		int height = 160;
+		int weight = 40;
+		int age = 20;
 
 	}
 

74122b540ce485126810e64fb680fb205957be84
user <user@es302n34>
2018-10-02 10:46:26 +0800

    commit

---
+++ myproject/src/com/tom/Hello.java
@@ -0,0 +1,17 @@
+package com.tom;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world");
+		String name = "Joyce";
+		int age = 20;
+		int height = 180;
+		int weight = 30;
+		
+
+	}
+
+}
